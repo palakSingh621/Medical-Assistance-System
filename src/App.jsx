735683eda97cc829c629ee770bb5b2ec26b94b35
ShivamGupta-5703 <shivgupta39693@gmail.com>
2024-06-29 16:07:34 +0530

    frontend

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Pages/Home';
+import Legal from './Pages/Legal';
+import Diagnos from './Pages/Diagnos';
+import NotFound from './Pages/NotFound';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/legal" element={<Legal />} />
+          <Route path="/diagnos" element={<Diagnos />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
